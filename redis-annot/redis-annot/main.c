4d8f104755f382c3ff78541ffae8e4078f699f4b
liubianshi <liubianshi@wacai.com>
2018-02-03 15:39:32 +0800

    revert

---
+++ redis-annot/redis-annot/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  redis-annot
+//
+//  Created by geksong on 2018/2/3.
+//  Copyright © 2018年 gekssourcecode. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
